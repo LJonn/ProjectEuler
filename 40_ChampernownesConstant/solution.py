cf6db1ea35f53ea911eb9117f3dcdab2a0893d93
ljon <lukas.jonusis2@gmail.com>
2023-06-17 14:55:13 +0300

    Improved performance

--- 40_ChampernownesConstant/solution.py
+++ 40_ChampernownesConstant/solution.py
@@ -1,14 +1,26 @@
-length_limit = 10**6+2
-number = "0."
+import time
+
+start_time = time.time()
+length_limit = 10**6
+digits = []
 i = 1
-while (len(number) < length_limit):
-    number += str(i)
+while (len(digits) < length_limit):
+    # digits += str(i) # bad code, concatenating slow
+    digits.extend(str(i))
     i += 1
 
 answer = 1
 for i in range(7):  # 7 things to multiply
-    answer *= int(number[10**i+1])
-    # i=0 | index=2
-    # i=1 | index=11
+    index = 10**i-1
+    # i=0 | index=0
+    # i=1 | index=9
+    # i=2 | index=99
+    answer *= int(digits[index])
 
+
+end_time = time.time()
+
+print(end_time-start_time)
+# old time 4.1 s
+# new time 0.1 s
 print(answer)
